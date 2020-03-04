981f1d2a60129adc7a545334bb541fc7c9bff5a3
Patrick McGrath <patrickmcgrath29@gmail.com>
2020-03-04 11:00:18 -0500

    Updating ssl-renew script

--- bin/ssl_renew.sh
+++ bin/ssl_renew.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-COMPOSE="/usr/local/bin/docker-compose --no-ansi"
-DOCKER="/usr/bin/docker"
-
-$COMPOSE run certbot renew --dry-run && $COMPOSE kill -s SIGHUP webserver
-$DOCKER system prune -af
+cd ../
+sudo docker-compose run certbot renew --dry-run && $COMPOSE kill -s SIGHUP webserver
+sudo docker system prune -af
