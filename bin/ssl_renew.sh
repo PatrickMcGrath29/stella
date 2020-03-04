514a98741037ff4a5571501fabe44c0d8739457d
Patrick McGrath <patrickmcgrath29@gmail.com>
2020-03-04 12:05:05 -0500

    fixing ssl renew script

--- bin/ssl_renew.sh
+++ bin/ssl_renew.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/patrickmcgrath/stella
-sudo docker-compose run certbot renew --dry-run && $COMPOSE kill -s SIGHUP webserver
+sudo docker-compose run certbot renew && sudo docker-compose kill -s SIGHUP webserver
 sudo docker system prune -af
