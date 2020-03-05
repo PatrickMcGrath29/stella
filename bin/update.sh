834e082c913b2c7c8c740c0593fb158625ab3461
Patrick McGrath <patrickmcgrath29@gmail.com>
2020-03-05 16:49:57 -0500

    Updating update.sh script

--- bin/update.sh
+++ bin/update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/patrickmcgrath/stella
 git pull
-sudo docker-compose down webserver nodejs
-sudo docker-compose up -d --no-deps --build webserver nodejs
+sudo docker-compose restart webserver
+sudo docker-compose up -d --no-deps --build nodejs
