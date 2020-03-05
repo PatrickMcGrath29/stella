cadbd38004346fd845b179f452fec498e70807cf
Patrick McGrath <patrickmcgrath29@gmail.com>
2020-03-05 16:27:21 -0500

    Updating nginx conf

--- bin/ssl_renew.sh
+++ bin/ssl_renew.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /home/patrickmcgrath/stella
-sudo docker-compose run certbot renew && sudo docker-compose kill -s SIGHUP webserver
+sudo docker-compose --no-ansi run certbot renew
+sudo docker-compose kill -s SIGHUP webserver
 sudo docker system prune -af
