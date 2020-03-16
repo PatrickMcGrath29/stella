21e009997d679a65f7396aa792fe446306938239
Patrick McGrath <patrickmcgrath29@gmail.com>
2020-03-16 13:16:12 -0400

    Updating path and whitelisting the alias path

--- api/routes/aliasRoutes.js
+++ api/routes/aliasRoutes.js
@@ -2,10 +2,10 @@
 module.exports = (app) => {
   const alias = require('../controllers/aliasController');
 
-  app.route('/')
+  app.route('/alias/')
     .post(alias.create_alias);
 
-  app.route('/:alias')
+  app.route('/alias/:alias')
     .get(alias.get_full_url)
     .delete(alias.delete_alias);
 };
